aaf7ca5eeb9a056044c17cf2e695c52a093850f2
Devansh <99159017+Devansh-Vaidya@users.noreply.github.com>
2023-01-22 04:03:38 -0500

    Edited the landing page, deleted unused js files

--- client-react/client/src/Components/SignUp/SignupBox.js
+++ client-react/client/src/Components/SignUp/SignupBox.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../Header";
 import Form from "./Form";
 import "./SignupBox.css";
 
 const SignupBox = () => {
 	return (
 		<div>
-			<Header />
 			<div className="signup-box container text-bg-light rounded-5">
 				<h3 className="text-center pt-2 mt-2 fw-semibold">Sign Up</h3>
 				<hr />
